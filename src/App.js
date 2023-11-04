c317803249c2f09b7ecef3908d9b01171174ffe8
bonganimasemola <bonganimasemola@icloud.com>
2023-11-04 10:11:29 +0300

    a few edits to components - API not working properly

--- src/App.js
+++ src/App.js
@@ -1,28 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { fetchBots } from './api'; // Import the fetchData function from api.js
+import { fetchBots } from './api'; 
+
 
 function App() {
-  // Create state to store the fetched data
+  
   const [bots, setBots] = useState([]);
 
   useEffect(() => {
-    // Fetch data when the component mounts
     fetchBots()
       .then(data => {
-        // Update the state with the fetched data
+        
         setBots(data.bots);
       })
       .catch(error => {
         console.error('Error fetching data:', error);
       });
-  }, []); // The empty dependency array means this effect runs only once when the component mounts
+  }, []); 
 
   return (
     <div className="App">
       <header className="App-header">
         <h1>Bot Battlr</h1>
-        {/* Render the fetched data here */}
+        
         <ul>
           {bots.map(bot => (
             <li key={bot.id}>
--- src/api.js
+++ src/api.js
@@ -1,8 +1,8 @@
-const API = 'http://localhost:8001/bots'; // Set your API base URL
+const API = 'http://localhost:8001/bots'; 
 
 const fetchBots = async () => {
   try {
-    const response = await fetch(`${API}/bots`); // Use the API constant to construct the URL
+    const response = await fetch(`${API}/bots`);
     if (!response.ok) {
       throw new Error(`Failed to fetch data: ${response.status} ${response.statusText}`);
     }
