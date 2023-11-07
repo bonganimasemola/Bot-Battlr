17f72069cef0e81aa637bb9fcf75b9a8af45790e
bonganimasemola <bonganimasemola@icloud.com>
2023-11-07 09:55:10 +0300

    some CSS edits

--- src/BotArmy.js
+++ src/BotArmy.js
@@ -11,12 +11,13 @@ export default function BotArmy({ enlistedBots, releaseBot }) {
       console.error('Error deleting bot:', error);
     }
   };
-
+console.log(enlistedBots);
   return (
-    <div className="container">
+    <div className="">
       <h1>The Bot Army</h1>
+      <div className='container'>
       {enlistedBots.map((bot) => (
-        <div key={bot.id}>
+        <div key={bot.id} style={{border:"2px solid black"}}>
           <img src={bot.avatar_url} alt={bot.name} />
           <p>
             <strong>Name:</strong> {bot.name} <strong>Bot Class:</strong> {bot.bot_class}
@@ -25,6 +26,7 @@ export default function BotArmy({ enlistedBots, releaseBot }) {
           <button onClick={() => handleDelete(bot)}>x</button> 
         </div>
       ))}
+      </div>
     </div>
   );
 }
